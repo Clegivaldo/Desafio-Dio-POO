fdc5c0f779ce3a15439039b62d6f9f90a611e08e
Clegivaldo <nekinho@live.com>
2023-04-12 22:17:12 -0300

    Update 3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -23,9 +25,28 @@ public class Main {
         mentoria1.setDescricao("descrição da mentoria java");
         mentoria1.setData(LocalDate.now());
 
+/*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria1);
+*/
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devClegivaldo = new Dev();
+        devClegivaldo.setNome("Clegivaldo");
+        devClegivaldo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos disponíveis:" + devClegivaldo.getConteudosInscritos());
+
+
+        Dev devJulia = new Dev();
+        devJulia.setNome("Júlia");
+        devJulia.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos disponíveis:" + devJulia.getConteudosInscritos());
     }
 }
